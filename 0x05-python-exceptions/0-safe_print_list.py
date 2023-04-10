0a94ac5035d50369b69773ab85c8bfd86469a6d8
Yungskillzdougie <douglaskache@gmail.com>
2023-04-10 03:49:53 -0700

    Print counts integers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,16 +3,19 @@ def safe_print_list(my_list=[], x=0):
 
     count = 0
 
-
     for i in range(x):
 
         try:
             print("{}".format(my_list[i], end='')
+                    
                     except:
+                    
                     break
                    
                     else:
+                    
                     count += 1
 
                     print()
+
                     return (count)
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for D in range(x):
+        try:
+            print("{:d}".format(my_list[D]), end='')
+        except TypeError:
+            pass
+        except Valueerror:
+            pass
+        else:
+
+            count += 1
+
+            print()
+            return (count)
